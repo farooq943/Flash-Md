4bcd5d4d3cb74b30e1baa40388f2bd0eb0ce5a19
farooq943 <162565404+farooq943@users.noreply.github.com>
2024-03-26 12:07:29 +0500

    Update youtube.js

--- commandes/youtube.js
+++ commandes/youtube.js
@@ -10,7 +10,7 @@ const yts1 = require("youtube-yts");
 zokou({
   nomCom: "play",
   categorie: "Search",
-  reaction: "💿"
+  reaction: "💿"🌠"
 }, async (origineMessage, zk, commandeOptions) => {
   const { ms, repondre, arg } = commandeOptions;
      
